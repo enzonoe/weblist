93021579624b3ad883db5fad2a9f6a73472f74b5
Konrad Kaminski <konrad.kaminski@htlhl.at>
2024-03-08 14:49:49 +0100

    Values in the ListTable.js list table adjusted

--- src/lists/ListTable.js
+++ src/lists/ListTable.js
@@ -3,35 +3,23 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-        field: 'age',
-        headerName: 'Age',
-        type: 'number',
-        width: 90,
-    },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+    { field: 'taskName', headerName: 'Task name', width: 200 },
+    { field: 'description', headerName: 'Description', width: 240 },
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 2, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 3, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 4, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 5, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 6, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 7, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 8, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 9, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 10, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 11, taskName: 'Grocerie List', description: 'List of my groceries'},
+    { id: 12, taskName: 'Grocerie List', description: 'List of my groceries'},
 ];
 
 export default function DataTable() {
@@ -40,13 +28,6 @@ export default function DataTable() {
             <DataGrid
                 rows={rows}
                 columns={columns}
-                initialState={{
-                    pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
-                    },
-                }}
-                pageSizeOptions={[5, 10]}
-                checkboxSelection
             />
         </div>
     );
