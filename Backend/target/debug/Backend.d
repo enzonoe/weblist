c6b32a0359c5d339ccb47047496dd89980f97de3
Konrad Kaminski <konrad.kaminski50@gmail.com>
2024-03-11 18:54:48 +0100

    Rust Project "Backend" added and React Project moved into "Frontend"

---
+++ Backend/target/debug/Backend.d
@@ -0,0 +1,1 @@
+/home/konrad/WebstormProjects/weblist/Backend/target/debug/Backend: /home/konrad/WebstormProjects/weblist/Backend/src/main.rs
---
+++ Backend/target/debug/deps/Backend-7f00b6eaba16fceb.d
@@ -0,0 +1,5 @@
+/home/konrad/WebstormProjects/weblist/Backend/target/debug/deps/Backend-7f00b6eaba16fceb: src/main.rs
+
+/home/konrad/WebstormProjects/weblist/Backend/target/debug/deps/Backend-7f00b6eaba16fceb.d: src/main.rs
+
+src/main.rs:
