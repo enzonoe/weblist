0df0d96efad7589575693014d7f000060820e310
Konrad Kaminski <konrad.kaminski50@gmail.com>
2024-03-11 19:14:38 +0100

    main.rs template code added

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use actix_web::{App, HttpServer};
+use dotenv::dotenv;
+use listenfd::ListenFd;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+    db::init();
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().configure(employees::init_routes));
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None => {
+            let host = env::var("HOST").expect("Please set host in .env");
+            let port = env::var("PORT").expect("Please set port in .env");
+            server.bind(format!("{}:{}", host, port))?
+        }
+    };
+    server.run().await
 }
